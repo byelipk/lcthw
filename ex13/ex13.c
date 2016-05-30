bfeb97962624c0142fcf0ed35612983c4fb273b8
byelipk <byelipk@gmail.com>
2016-05-30 11:50:26 -0400

    ex13

--- ex13/ex13.c
+++ ex13/ex13.c
@@ -6,6 +6,9 @@ void log_this(char letter)
   printf("%d: %c\n", letter, toupper(letter));
 }
 
+// NOTE
+// This program takes the first command line argument
+// and prints out all of the vowels.
 int main(int argc, char *argv[])
 {
   if (argc != 2) {
@@ -18,8 +21,13 @@ int main(int argc, char *argv[])
 
   for (size_t i = 0; argv[1][i] != '\0'; i++) {
     char letter = argv[1][i];
+    // char lower  = tolower(argv[1][i]);
+    // char upper  = toupper(argv[1][i]);
+
+    // printf("%c %c %c\n", letter, upper, lower);
 
     switch (letter) {
+      // Fallthrough
       case 'a':
       case 'A':
       case 'e':
@@ -38,6 +46,7 @@ int main(int argc, char *argv[])
           log_this(letter);
         }
 
+      default:
         break;
     }
   }
